2ff9413881f9791099483a88a236efd2666786e2
WormOn <1282790991@qq.com>
2015-10-08 21:09:08 +0800

    Create funtions.c

---
+++ funtions.c
@@ -0,0 +1,70 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<conio.h>
+#include<string.h>
+#include"functions.h"
+
+void add()
+{
+	printf("加法\n");
+}
+void minus()
+{
+	printf("减法\n");
+}
+void multiply()
+{
+
+}
+void divided()
+{
+
+}
+void sinx()
+{
+
+}
+void arcsin()
+{
+
+}
+void cosx()
+{
+
+}
+void arccos()
+{
+
+}
+void 	tanx()
+{
+
+}
+void arctan()
+{
+
+}
+void 	logx()
+{
+
+}
+void log10x()
+{
+
+}
+void powx()
+{
+
+}
+void expx()
+{
+
+}
+void sqrtx()
+{
+
+}
+void tuichu()
+{
+	printf("退出\n");
+}
